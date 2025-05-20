2e612cc4ea51d8fa78e3eb83eb8ad768d59dcc3f
DTB <183888664+DoTheBetter@users.noreply.github.com>
2025-05-20 14:16:12 +0800

    feat(rsync): 更新s6-overlay至3.2.1.0

--- rsync/rootfs/etc/s6-overlay/s6-rc.d/init/00-banner.sh
+++ rsync/rootfs/etc/s6-overlay/s6-rc.d/init/00-banner.sh
@@ -4,7 +4,7 @@ echo "==========================================="
 cat /etc/s6-overlay/s6-rc.d/init/00-banner
 echo " "
 echo " "
-echo "Rsync版本: $(rsync --version | sed -n 's/.*\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p')"
+echo "Rsync版本：RSYNC_VER"
 echo "SSH服务：$([ "$SSH" = "true" ] && echo "[已启用]" || echo "<未启用>") | 计划任务：$([ "$CRON" = "true" ] && echo "[已启用]" || echo "<未启用>")"
 echo "Rsync守护进程：$([ "$RSYNC" = "true" ] && echo "[已启用]" || echo "<未启用>") | Lsyncd守护进程：$([ "$LSYNCD" = "true" ] && echo "[已启用]" || echo "<未启用>")"
 echo "==========================================="
